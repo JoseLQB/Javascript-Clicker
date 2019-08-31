474fd998ea516d69149789437fdcccac77a635a8
Jose L Quintanilla Blanco <JoseLQB@users.noreply.github.com>
2019-08-31 15:45:17 +0200

    ...

--- clicks.js
+++ clicks.js
@@ -1,7 +1,10 @@
+//variables
 var clicks = 0;
-var inventario = [0, 0, 0];
-var clicksProduce = [1, 2, 4];
-var precioProducto = [100, 200, 400];
+var inventario = [0, 0, 0, 0, 0, 0];
+var clicksProduce = [1, 2, 4, 10, 20, 40];
+var precioProducto = [100, 200, 400, 600, 1000, 1600];
+
+//acciones
 function clic() {
     clicks++;
 }
@@ -15,7 +18,7 @@ function comprar(objeto) {
         clicks -= precioProducto[objeto];
     }
     else {
-        console.log("NO TIENES DINERO");
+        console.log("NO TIENES SUFICIENTE DINERO");
     }
 }
 function producir() {
@@ -28,9 +31,13 @@ function render() {
     document.getElementById("inventario").innerHTML =
         `Cursores: ${inventario[0]}<br>
     Power x2: ${inventario[1]}<br>
-    Power x10: ${inventario[2]}`;
+    Power x10: ${inventario[2]}<br>
+    Power x50: ${inventario[3]}<br>
+    Power x100: ${inventario[4]}<br>
+    Power x160: ${inventario[5]}`;
 }
-//--------------------------------
+
+//refresco de pantalla
 var FPSProduce = 1;
 setInterval(function () {
     producir();
